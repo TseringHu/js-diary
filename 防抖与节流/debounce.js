a70e61fc60d450da082306f89fd9db6edf7d4cc4
huzelin <1350182355@qq.com>
2021-12-26 22:25:10 +0800

    防抖

---
+++ 防抖与节流/debounce.js
@@ -0,0 +1,11 @@
+function debounce (fn, delay) {
+    let timer = null
+    return () => {
+        if (timer) {
+            clearTimeout(timer)
+        }
+        timer = setTimeout(() => {
+            fn()
+        }, delay)
+    }
+}
