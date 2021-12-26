5def649b6d46fd3b507f782d479ac2bc1f25981d
huzelin <1350182355@qq.com>
2021-12-26 22:26:25 +0800

    generator

---
+++ Promise/generator.js
@@ -0,0 +1,9 @@
+var fetch = require('node-fetch');
+
+function* gen(){
+  var url = 'https://api.github.com/users/github';
+  var result = yield fetch(url);
+  console.log(result.bio);
+}
+
+console.log('gen() :>> ', gen())
